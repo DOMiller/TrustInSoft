b8aa00f48c9f3b967e702cf6196e455474de6b8a
DOMiller <38739466+DOMiller@users.noreply.github.com>
2018-05-01 12:50:12 -0400

    Update oversized-shift.c

--- examples/04-Oversized-Shift/oversized-shift.c
+++ examples/04-Oversized-Shift/oversized-shift.c
@@ -4,7 +4,7 @@ int main(void)
     int i, sum;
 
     sum = 2;
-    for (i = 0; i <= 4; i++)
+    for (i = 0; i < 4; i++)
         /* TrustInSoft Analyzer emits an alarm concerning the left-shift
            expression '2 << i'. In particular, the analyzer is not able to
            guarantee '0 ≤ (int)(2<<i) < 32' valid (on x86 architecture).
